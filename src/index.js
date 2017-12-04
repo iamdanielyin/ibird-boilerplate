9c26f0e2bcc9af3aa14334659c90c14df9665891
yinfxs <yinfxs@gmail.com>
2017-12-04 18:08:22 +0800

    fix bugs

--- src/index.js
+++ src/index.js
@@ -3,19 +3,22 @@
  */
 
 const path = require('path');
-const ibird = require('ibird');
 const koaLogger = require('koa-logger');
 const session = require('koa-session');
+
+const ibird = require('ibird');
 const i18nAddon = require('ibird-i18n');
 const mongooseAddon = require('ibird-mongoose');
 const accountsAddon = require('ibird-accounts');
 const loggerAddon = require('ibird-logger');
 const openAddon = require('ibird-open');
+
 const assign = require('ibird-utils').assign;
 const env = process.env.NODE_ENV || 'development';
 const config = require(`./config/environments/${env}`);
 const appName = 'myApp';
 
+// 初始化应用实例
 const app = ibird.newApp(assign({
     env,
     name: appName,
@@ -25,11 +28,12 @@ const app = ibird.newApp(assign({
     mongo: `mongodb://localhost/${appName}`,
 }, config));
 
-app.keys = [appName];
-
+// 引用koa中间件
 app.use(koaLogger());
+app.keys = [appName];
 app.use(session({ key: appName + ':sess' }, app));
 
+// 引用ibird插件
 app.import(openAddon);
 app.import(i18nAddon, { localesDir: path.join(__dirname, 'config/locales') });
 app.import(loggerAddon, { logDir: path.join(__dirname, 'logs') });
@@ -38,7 +42,7 @@ app.import(mongooseAddon, {
 });
 app.import(accountsAddon, {
     tokenKey: 'ibird_token',
-    secretOrPrivateKey: 'ibird_app_secret',
+    secretOrPrivateKey: appName,
     payloadGetter: function (ctx) {
         const { username, password } = ctx.query;
         return (username === 'yinfxs' && password === '123456') ? {
@@ -50,10 +54,11 @@ app.import(accountsAddon, {
     },
     whitelists: [
         'POST /login',
-        '\w* /'
+        /^GET\s*\/$/
     ]
 });
 
+// 挂载相关目录
 app.useDir(path.join(__dirname, 'middleware'));
 app.mountDir(path.join(__dirname, 'routes'));
 app.modelDir(path.join(__dirname, 'models'));
