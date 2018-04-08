fa696d48fd564e58e9b121308a07810db75bccc0
yinfxs <yinfxs@gmail.com>
2018-04-08 15:30:43 +0800

    added test

---
+++ test/index.test.js
@@ -0,0 +1,15 @@
+const assert = require('assert');
+
+describe('ibird-boilerplate', function () {
+    it('test1', function () {
+        assert.deepEqual(1, 1, '1!=1 :)');
+    });
+
+    it('test2', function () {
+      assert.deepEqual(2, 2, '2!=2 :)');
+    });
+
+    it('test3', function () {
+      assert.deepEqual(3, 3, '3!=3 :)');
+    });
+});
