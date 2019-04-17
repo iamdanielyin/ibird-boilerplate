bf8b09e2d0d2288d1e014a491ec9bb7b16454e04
yinfxs <yinfxs@gmail.com>
2019-04-17 16:50:20 +0800

    update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,11 @@
  * 模块依赖
  */
 
-const path = require('path');
-const execSync = require('child_process').execSync;
 const gulp = require("gulp");
 const babel = require("gulp-babel");
 const uglify = require('gulp-uglify');
 const sequence = require('gulp-sequence');
+const pipeline = require('readable-stream').pipeline;
 const del = require('del');
 const dest = 'dist';
 
@@ -18,26 +17,31 @@ gulp.task('clean', function () {
 
 // 编译服务端
 gulp.task('build:server', function () {
-    return gulp.src('src/**/*.js')
-        .pipe(babel({
-            presets: ['env'],
-            plugins: ['transform-runtime']
-        }))
-        .pipe(uglify({
+    return pipeline(
+        gulp.src('src/**/*.js'),
+        babel({
+            presets: ['@babel/env'],
+            plugins: ['@babel/transform-runtime']
+        }),
+        uglify({
             sourceMap: {
                 url: 'inline'
             }
-        }))
-        .pipe(gulp.dest(dest));
+        }),
+        gulp.dest(dest)
+    );
 });
 
 // 复制其余文件
 gulp.task('copy:others', function () {
-    return gulp.src([
-        'package.json',
-        'Dockerfile',
-        'src/**/*.json'
-    ]).pipe(gulp.dest('dist'));
+    return pipeline(
+        gulp.src([
+            'package.json',
+            'Dockerfile',
+            'src/**/*.json'
+        ]),
+        gulp.dest('dist')
+    );
 });
 
 gulp.task('default', sequence('clean', ['build:server', 'copy:others']));
